f99ebeda8775dafaa5d9777979e7e3b77f52c3ef
JKurzer <jake.kurzer@gmail.com>
2024-08-20 19:08:22 -0400

    Add module

--- Source/SkeletonKey/Private/SkeletonKey.cpp
+++ Source/SkeletonKey/Private/SkeletonKey.cpp
@@ -2,18 +2,18 @@
 
 #include "SkeletonKey.h"
 
-#define LOCTEXT_NAMESPACE "FSkeletonKey"
+#define LOCTEXT_NAMESPACE "FSkeletonKeyModule"
 
 
 
-void FSkeletonKey::StartupModule()
+void FSkeletonKeyModule::StartupModule()
 {
 	// we have some stuff to be mindful of that makes isolating Barrage and the Barrage types a good idea, namely, packing pragmas.
 	// https://youtu.be/3OzPQOKFBQU?si=ebfPc14JuPraTvx5&t=81
 	// right now, I need to decide how much isolation I want.
 }
 
-void FSkeletonKey::ShutdownModule()
+void FSkeletonKeyModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
@@ -21,4 +21,4 @@ void FSkeletonKey::ShutdownModule()
 
 #undef LOCTEXT_NAMESPACE
 	
-IMPLEMENT_MODULE(FSkeletonKey, SkeletonKey)
+IMPLEMENT_MODULE(FSkeletonKeyModule, SkeletonKey)
--- Source/SkeletonKey/Public/SkeletonKey.h
+++ Source/SkeletonKey/Public/SkeletonKey.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FSkeletonKey: public IModuleInterface
+class FSkeletonKeyModule: public IModuleInterface
 {
 public:
 
